44f56ee83fbfbf2872e6f22a1775b0454ea90dfd
danielVg <danielveragi@gmail.com>
2019-06-01 14:37:37 +0200

    Home layout

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -8,6 +9,26 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Find peers"),
+      ),
+      // body: Row(
+      //  children: <Widget>[
+
+      //  ], 
+      // )
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: [
+          BottomNavigationBarItem(
+              icon: new Icon(MdiIcons.home), title: new Text("Home")),
+          BottomNavigationBarItem(
+              icon: new Icon(MdiIcons.compass), title: new Text("Discover")),
+          BottomNavigationBarItem(
+              icon: new Icon(MdiIcons.account), title: new Text("Account")),
+        ],
+      ),
+    );
   }
 }
