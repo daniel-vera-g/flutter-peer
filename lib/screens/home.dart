a6f438e1b3717de1e7589feffb7d12a71fb042bb
redsolver <info@redsolver.net>
2019-06-01 15:12:08 +0200

    some pages

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutterpeer/screens/discover.dart';
+import 'package:flutterpeer/screens/home_page.dart';
+import 'package:flutterpeer/screens/profile.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -7,6 +10,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int index = 1;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,18 +21,26 @@ class _HomeScreenState extends State<HomeScreen> {
       // body: Row(
       //  children: <Widget>[
 
-      //  ], 
+      //  ],
       // )
+      body: index == 0
+          ? DiscoverPage()
+          : index == 1 ? OverviewPage() : ProfilePage(),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
+        currentIndex: index,
         items: [
-          BottomNavigationBarItem(
-              icon: new Icon(MdiIcons.home), title: new Text("Home")),
           BottomNavigationBarItem(
               icon: new Icon(MdiIcons.compass), title: new Text("Discover")),
           BottomNavigationBarItem(
+              icon: new Icon(MdiIcons.home), title: new Text("Home")),
+          BottomNavigationBarItem(
               icon: new Icon(MdiIcons.account), title: new Text("Account")),
         ],
+        onTap: (i) {
+          setState(() {
+            index = i;
+          });
+        },
       ),
     );
   }
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OverviewPage extends StatefulWidget {
+  @override
+  _OverviewPageState createState() => _OverviewPageState();
+}
+
+class _OverviewPageState extends State<OverviewPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Home Page'),
+    );
+  }
+}
